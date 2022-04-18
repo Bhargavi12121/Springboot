4d61eebe6bf8ec5603140c6bfd1c0cf1c925b9cc
Bhargavivb <bhargaviburry123@gmail.com>
2022-04-18 13:49:21 +0530

    dockerfiles added

--- Student/src/main/java/com/wipro/rabbitmq/RabbitMqConfig.java
+++ Student/src/main/java/com/wipro/rabbitmq/RabbitMqConfig.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -13,8 +14,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.web.client.RestTemplate;
 
+@EnableRabbit
 @Configuration
 public class RabbitMqConfig {
 
